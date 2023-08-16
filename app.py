1b94412fa3571a37b75cfaf79fb9ae8c99afebd4
mind-set09 <141085830+mind-set09@users.noreply.github.com>
2023-08-16 14:39:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,78 +1,79 @@
-# Imports
-import hikari
-import lightbulb
-import miru
+import discord
+from discord.ext import commands
+from discord_components import DiscordComponents, Button, ButtonStyle
+import os
 
-import sqlalchemy
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+bot = commands.Bot(command_prefix="!")
+DiscordComponents(bot)
 
-# Bot setup
-bot = lightbulb.BotApp(token=TOKEN)
-bot.load_extensions_from("./extensions")
+active_tickets = {}
 
-# Load bot token from environment variable
-bot_token = os.environ["BOT_TOKEN"]
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
 
-# Database 
-Base = declarative_base()
+@bot.command()
+async def create_ticket(ctx):
+    embed = discord.Embed(title="New Ticket", description="Click the button to create a new ticket.", color=discord.Color.blue())
+    ticket_button = Button(style=ButtonStyle.blue, label="Create Ticket", custom_id="create_ticket")
+    await ctx.send(embed=embed, components=[ticket_button])
 
-class Ticket(Base):
-  __tablename__ = 'tickets'
-  
-  id = Column(Integer, primary_key=True)
-  title = Column(String)
-  status = Column(String)
-  
-# Persistent view for ticket creation  
-@bot.command
-@lightbulb.command('tickets', 'Ticket system commands')
-@lightbulb.implements(lightbulb.SlashCommandGroup)
-async def tickets(ctx):
+@bot.event
+async def on_button_click(ctx):
+    if ctx.custom_id == "create_ticket":
+        ticket_number = len(active_tickets) + 1
+        active_tickets[ticket_number] = {
+            "author_id": ctx.author.id,
+            "status": "Open"
+        }
 
-  persistent_view = miru.View()
-  persistent_view.add_item(
-    miru.Button(label="Create Ticket", custom_id="create_ticket")
-  )
-  await ctx.respond(views=persistent_view)
-  
-# Modal for ticket creation  
-@bot.component()
-async def create_ticket_modal(ctx):
+        embed = discord.Embed(title=f"Ticket #{ticket_number}", description="Your ticket has been created.", color=discord.Color.green())
+        embed.add_field(name="Status", value="Open", inline=True)
+        embed.add_field(name="Assigned to", value="Unassigned", inline=True)
+        embed.add_field(name="Description", value="No description provided.", inline=False)
 
-  modal = miru.Modal("Create Ticket")
-  
-  title_input = miru.TextInput(label="Title")
-  desc_input = miru.TextInput(label="Description")
-  
-  modal.add_input(title_input)
-  modal.add_input(desc_input)
-  
-  await ctx.respond(modal=modal)
-  
-# Ticket created embed   
-@bot.component()  
-async def ticket_created(ctx):
+        close_button = Button(style=ButtonStyle.red, label="Close Ticket", custom_id=f"close_ticket_{ticket_number}")
+        assign_button = Button(style=ButtonStyle.grey, label="Assign to Me", custom_id=f"assign_ticket_{ticket_number}")
 
-  embed = hikari.Embed(title=TITLE, description=DESC)
-  
-  await ctx.edit_response(embed=embed)
-  
-# Approval buttons 
-approve_button = miru.Button(label="Approve")
-deny_button = miru.Button(label="Deny")
+        await ctx.send(embed=embed, components=[close_button, assign_button])
 
-await ctx.edit_response(components=[approve_button, deny_button])
+@bot.event
+async def on_button_click(ctx):
+    if ctx.custom_id.startswith("close_ticket_"):
+        ticket_number = int(ctx.custom_id.split("_")[2])
+        ticket = active_tickets.get(ticket_number)
 
-# Logging
-import logging
+        if ticket and ticket["author_id"] == ctx.author.id:
+            ticket["status"] = "Closed"
+            await ctx.edit_origin(embed=generate_ticket_embed(ticket_number))
+            await ctx.send("Ticket closed.")
+        else:
+            await ctx.send("You don't have permission to close this ticket.")
 
-logger = logging.getLogger('bot')
-logger.setLevel(logging.INFO)
+    elif ctx.custom_id.startswith("assign_ticket_"):
+        ticket_number = int(ctx.custom_id.split("_")[2])
+        ticket = active_tickets.get(ticket_number)
 
-file_handler = logging.FileHandler('bot.log')
-logger.addHandler(file_handler) 
+        if ticket:
+            ticket["assigned_to"] = ctx.author.id
+            await ctx.edit_origin(embed=generate_ticket_embed(ticket_number))
+            await ctx.send("Ticket assigned to you.")
+        else:
+            await ctx.send("Ticket not found.")
 
-logger.info("Bot started")
+def generate_ticket_embed(ticket_number):
+    ticket = active_tickets.get(ticket_number)
+    if not ticket:
+        return None
 
-bot.run()
+    embed = discord.Embed(title=f"Ticket #{ticket_number}", color=discord.Color.green())
+    embed.add_field(name="Status", value=ticket["status"], inline=True)
+    
+    assigned_to = bot.get_user(ticket.get("assigned_to", 0))
+    embed.add_field(name="Assigned to", value=assigned_to.name if assigned_to else "Unassigned", inline=True)
+
+    embed.add_field(name="Description", value="No description provided.", inline=False)
+    return embed
+
+
+bot.run(os.environ['TOKEN'])
