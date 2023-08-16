3bd49b6cfb49e334af1307c1b259a8205e80d428
mind-set09 <141085830+mind-set09@users.noreply.github.com>
2023-08-15 23:12:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
 import disnake
-from disnake.ext import commands
 import psutil
 import platform
-import time
+from disnake.ext import commands
 import datetime
+import os
 
-intents = disnake.Intents.default()
-intents.typing = False
-intents.presences = False
-
-bot = commands.Bot(command_prefix="/", intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user.name} - {bot.user.id}")
+bot = commands.Bot(command_prefix="/")
 
 @bot.slash_command()
-async def botinfo(ctx):
+async def botinfo(inter):
     embed = disnake.Embed(
         title="Bot Information 🤖",
         description="Here's some information about the bot!",
@@ -31,44 +23,26 @@ async def botinfo(ctx):
     cpu_usage = psutil.cpu_percent()
     embed.add_field(name="CPU Usage 💻", value=f"{cpu_usage}%")
 
-    memory = psutil.virtual_memory().percent
-    embed.add_field(name="Memory Usage 🧠", value=f"{memory}%")
+    memory_info = psutil.virtual_memory()
+    embed.add_field(name="Memory Usage 🧠", value=f"{memory_info.percent}%")
 
-    server_count = len(bot.guilds)
-    embed.add_field(name="Server Count 🌐", value=f"{server_count} servers")
-
-    uptime = datetime.datetime.now() - bot.start_time
-    uptime_str = f"{uptime.days} days, {uptime.seconds // 3600} hours, {(uptime.seconds // 60) % 60} minutes"
-    embed.add_field(name="Uptime ⏲️", value=uptime_str)
+    os_info = f"{platform.system()} {platform.release()}"
+    embed.add_field(name="Operating System 🖥️", value=os_info)
 
     python_version = platform.python_version()
     embed.add_field(name="Python Version 🐍", value=python_version)
 
-    await ctx.send(embed=embed)
+    invite_button = disnake.ui.Button(
+        label="Invite Bot 🤖",
+        emoji="🤖",
+        url=f"https://discord.com/oauth2/authorize?client_id={bot.user.id}&permissions=0&scope=bot",
+        style=disnake.ButtonStyle.link
+    )
 
-def get_live_info():
-    while True:
-        cpu_percent = psutil.cpu_percent()
-        memory = psutil.virtual_memory()
-        network_info = psutil.net_io_counters()
-        python_version = platform.python_version()
-        platform_info = platform.platform()
+    view = disnake.ui.View()
+    view.add_item(invite_button)
 
-        live_info = {
-            "CPU Usage": f"{cpu_percent}%",
-            "Memory Usage": f"{memory.percent}%",
-            "Network Sent": f"{network_info.bytes_sent / 1024:.2f} KB",
-            "Network Received": f"{network_info.bytes_recv / 1024:.2f} KB",
-            "Python Version": python_version,
-            "Platform": platform_info
-        }
-
-        print("Live Bot Information:")
-        for key, value in live_info.items():
-            print(f"{key}: {value}")
-
-        time.sleep(5)  # Update every 5 seconds
+    await inter.response.send_message(embed=embed, view=view)
 
 if __name__ == "__main__":
-    bot.loop.create_task(get_live_info())
-    bot.run("MTE0MTE4MjQ3MTQwOTUwNDMwNg.Gx68d-.SmdzQh2OC3fBmDaxb4fORptimR0hlrzjmtymZ8")
+    bot.run(os.environ["BOT_TOKEN"])
