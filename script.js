4905fe32e9a3d1f166a489a7b8d51a4d27658ca7
mind-set09 <141085830+mind-set09@users.noreply.github.com>
2023-08-16 00:08:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,41 @@
-// Function to update the terminal with error messages
-function updateTerminal(message) {
-  const terminalOutput = document.getElementById('terminal-output');
-  terminalOutput.textContent += message + '\n';
-  terminalOutput.scrollTop = terminalOutput.scrollHeight;
-}
-
-// Function to update the bot information in the dashboard
-function updateBotInfo(botInfo) {
-  const pingElement = document.getElementById('ping');
-  const cpuUsageElement = document.getElementById('cpu-usage');
-  const memoryUsageElement = document.getElementById('memory-usage');
-  const osInfoElement = document.getElementById('os-info');
-  const pythonVersionElement = document.getElementById('python-version');
-  
-  pingElement.textContent = botInfo.ping + ' ms';
-  cpuUsageElement.textContent = botInfo.cpu_usage + '%';
-  memoryUsageElement.textContent = botInfo.memory_usage + '%';
-  osInfoElement.textContent = botInfo.os_info;
-  pythonVersionElement.textContent = botInfo.python_version;
-}
-
-// Dummy data for testing
-const dummyBotInfo = {
-  ping: '25',
-  cpu_usage: '10',
-  memory_usage: '70',
-  os_info: 'Linux 5.4.0-1059-azure',
-  python_version: '3.8.10'
-};
-
-// Update the bot information on page load
-window.addEventListener('DOMContentLoaded', () => {
-  updateBotInfo(dummyBotInfo);
-});
-
-// Example: Update bot information when a button is clicked
-document.getElementById('refresh-btn').addEventListener('click', async () => {
+// Fetch server count and total users
+async function fetchServerInfo() {
   try {
-    // Fetch bot information from the server
-    // Replace this with your actual API endpoint
-    const response = await fetch('/api/botinfo');
+    const response = await fetch('/server_info'); // Replace with the endpoint for fetching server info
     const data = await response.json();
-    
-    // Update the bot information on the page
-    updateBotInfo(data);
+
+    const serverCount = data.serverCount;
+    const totalUsers = data.totalUsers;
+
+    document.getElementById('server-count').textContent = serverCount;
+    document.getElementById('total-users').textContent = totalUsers;
   } catch (error) {
-    // Handle any errors and display in the terminal
-    updateTerminal('Error: ' + error.message);
+    console.error(error);
   }
+}
+
+// Fetch bot statistics
+async function fetchBotStats() {
+  try {
+    const response = await fetch('/bot_stats'); // Replace with the endpoint for fetching bot stats
+    const data = await response.json();
+
+    document.getElementById('ping').textContent = `${data.ping} ms`;
+    document.getElementById('cpu-usage').textContent = `${data.cpuUsage}%`;
+    document.getElementById('memory-usage').textContent = `${data.memoryUsage}%`;
+    document.getElementById('os-info').textContent = data.osInfo;
+    document.getElementById('python-version').textContent = data.pythonVersion;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Refresh bot info when button is clicked
+document.getElementById('refresh-btn').addEventListener('click', () => {
+  fetchServerInfo();
+  fetchBotStats();
 });
+
+// Initial fetch when the page loads
+fetchServerInfo();
+fetchBotStats();
