c470abe46d1cce613653daec2e1adfe5b0ad0c86
mind-set09 <141085830+mind-set09@users.noreply.github.com>
2023-08-16 15:47:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,123 +1,134 @@
-import disnake
-from disnake.ext import commands, tasks
-from disnake import ApplicationCommandInteraction, SelectMenu, SelectOption, Button
 import os
+from datetime import datetime
 
-intents = disnake.Intents.all()
-bot = commands.Bot(intents=intents)
+import disnake
+from disnake import SlashCommandGroup, Option 
+from disnake.ext import commands, tasks
+import sqlite3
 
-class TicketStatus(disnake.Enum):
-    OPEN = 'Open'
-    IN_PROGRESS = 'In Progress'
-    CLOSED = 'Closed'
+info_commands = SlashCommandGroup("info", "System information commands")
 
-class TicketPriority(disnake.Enum):
-    HIGH = 'High'
-    MEDIUM = 'Medium'
-    LOW = 'Low'
+@info_commands.command()
+async def system(ctx):
+
+  cpu = psutil.cpu_percent()
+  mem = psutil.virtual_memory()
+  disk = psutil.disk_usage('/')
+  net_io = psutil.net_io_counters()
+
+  embed = disnake.Embed(title="System Information")
+
+  embed.add_field(name="CPU", value=f"{cpu}% Usage")
+
+  embed.add_field(name="RAM", value=f"{mem.used/1024**2:,.2f}MB Used")  
+
+  embed.add_field(name="Disk", value=f"{disk.used/1024**2:,.2f}GB Used")
+
+  embed.add_field(name="Network", 
+                  value=f"Sent: {net_io.bytes_sent/1024**2:,.2f}MB Received: {net_io.bytes_recv/1024**2:,.2f}MB")
+
+  if gpu := psutil.gpu():
+    embed.add_field(name="GPU", value=gpu.name)
+      
+  view = disnake.ui.View()
+
+  support_btn = disnake.ui.Button(
+    label="Support Server", 
+    url="https://discord.gg/WsGgGUyfTV"
+  )
+
+  view.add_item(support_btn)
+
+  invite_btn = disnake.ui.Button(
+    label="Invite Bot",
+    url=f"https://discord.com/oauth2/authorize?client_id={bot.user.id}&scope=applications.commands%20bot" 
+  )
+
+  view.add_item(invite_btn)
+
+  await ctx.respond(embed=embed, view=view)
+
+bot.tree.add_command(info_commands)
 
 class Ticket:
-    def __init__(self, priority, category, author_id, status=TicketStatus.OPEN):
-        self.priority = priority
-        self.category = category
-        self.author_id = author_id
-        self.status = status
-    
-    def to_embed(self):
-        embed = disnake.Embed(
-            title=f"Ticket Information",
-            description=f"Priority: {self.priority}\nCategory: {self.category}\nStatus: {self.status}",
-            color=disnake.Color.blurple()
-        )
-        
-        embed.set_footer(text=f"Author ID: {self.author_id}")
-        
-        return embed
+  def __init__(self, id, description, status, due_date=None):
+    self.id = id
+    self.description = description
+    self.status = status
+    self.due_date = due_date
+
+  @classmethod
+  def from_dict(cls, data):
+    return cls(data['id'], data['description'], data['status'], data['due_date'])
+
+class Database:
+  def __init__(self):
+    self.conn = sqlite3.connect('tickets.db')
+    self.cur = self.conn.cursor()
+
+  def create_tables(self):
+    self.cur.execute("""
+      CREATE TABLE IF NOT EXISTS tickets (
+        id INTEGER PRIMARY KEY, 
+        description TEXT,
+        status TEXT, 
+        due_date TEXT
+      )
+    """)
+
+  async def create_ticket(self, ticket):
+    self.cur.execute("INSERT INTO tickets VALUES (?, ?, ?, ?)", 
+      (ticket.id, ticket.description, ticket.status, ticket.due_date))
+    self.conn.commit()
+
+  # Other database methods
+
+  async def close(self):
+    self.conn.close()
+
+bot = commands.Bot(command_prefix='!')
+db = Database()
+
+ticket_commands = SlashCommandGroup("ticket", "Ticket commands")
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user}')
+  db.create_tables()
+  print("Bot ready")
 
-@bot.slash_command()
-async def create(ctx):
-    priority_select = SelectMenu(
-        placeholder='Select priority...',
-        options=[
-            SelectOption(
-                label='High',
-                value=TicketPriority.HIGH
-            ),
-            SelectOption(
-                label='Medium',
-                value=TicketPriority.MEDIUM
-            ),
-            SelectOption(
-                label='Low',
-                value=TicketPriority.LOW
-            )
-        ]
-    )
+@ticket_commands.command()
+async def create(ctx, description: str, due_date: Option(datetime, "Leave empty for none")):
+  ticket = Ticket(len(db.get_tickets()) + 1, description, "open", due_date)
+  await db.create_ticket(ticket)
+  await ctx.respond(f"Ticket {ticket.id} created")
 
-    category_select = SelectMenu(
-        placeholder='Select category...',
-        options=[
-            SelectOption(
-                label='Bug',
-                value='bug'
-            ),
-            SelectOption(
-                label='Feature Request',
-                value='feature'
-            ),
-            SelectOption(
-                label='Support',
-                value='support'
-            )
-        ]
-    )
+@ticket_commands.command() 
+async def list(ctx):
+  tickets = db.get_tickets()
+  embed = disnake.Embed(title="Open Tickets")
+  for ticket in tickets:
+    embed.add_field(name=ticket.id, value=f"{ticket.description} - Due: {ticket.due_date}")
+  await ctx.respond(embed=embed)
 
-    await ctx.send('Choose priority and category:',
-                   components=[priority_select, category_select])
+# Other commands
 
-@bot.component()
-async def priority_select(ctx, select):
-    author_id = ctx.author.id
-    priority = select.values[0]
+@tasks.loop(minutes=15)
+async def check_tickets():
 
-    category_select = SelectMenu(
-        placeholder='Select category...',
-        options=[
-            SelectOption(
-                label='Bug',
-                value='bug'
-            ),
-            SelectOption(
-                label='Feature Request',
-                value='feature'
-            ),
-            SelectOption(
-                label='Support',
-                value='support'
-            )
-        ]
-    )
+  now = datetime.now()
 
-    await ctx.edit_origin(components=[category_select])
+  tickets = db.get_tickets()
 
-@bot.component()
-async def category_select(ctx, select):
-    author_id = ctx.author.id
-    priority = ctx.message.components[0].values[0]
-    category = select.values[0]
+  for ticket in tickets:
+    if ticket.status == "open" and ticket.due_date < now:
+      ticket.status = "overdue"
+      await db.update_ticket(ticket)
 
-    ticket = Ticket(priority, category, author_id)
+  await bot.get_channel(channel_id).send("Overdue tickets!")
 
-    # Implement ticket creation logic here
-    # For example, you might want to save the ticket to a database
-    # and send a confirmation message or embed
-    
-    # Placeholder confirmation message
-    confirmation_message = f"Ticket created!\nPriority: {priority}\nCategory: {category}"
-    await ctx.send(confirmation_message)
+@bot.event
+async def on_slash_command_error(ctx, error):
+  await ctx.send(f"Error: {error}")
 
-bot.run(os.environ['TOKEN'])
+bot.tree.add_command(ticket_commands)
+bot.run(os.getenv("TOKEN"))
